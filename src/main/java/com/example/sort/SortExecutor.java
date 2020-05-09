56e5a3e76750d72bb68c022c59d80cdcd803590c
farazahmedsyed <syedfaraz_ahmed@ymail.com>
2020-05-10 00:20:17 +0500

    Added sl4j logging.

--- src/main/java/com/example/sort/SortExecutor.java
+++ src/main/java/com/example/sort/SortExecutor.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Arrays;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Slf4j
 public class SortExecutor {
 
     public static void execute() {
@@ -15,10 +16,10 @@ public class SortExecutor {
 
             SortOddThenEven.execute(arr);
 
-            System.out.println(Arrays.toString(arr));
+            log.info(Arrays.toString(arr));
         } catch (Exception e) {
-            System.out.println("System Error: " + e.getMessage());
-            System.out.println("Program Exit");
+            log.error("System Error: " + e.getMessage());
+            log.info("Program Exit");
         }
 
     }
--- src/main/java/com/example/sort/UserInput.java
+++ src/main/java/com/example/sort/UserInput.java
@@ -2,10 +2,12 @@ package com.example.sort;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Scanner;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Slf4j
 public class UserInput {
 
     public static final String ERROR_INVALID_NUMBER = "You didn't provide enough numbers";
@@ -14,18 +16,18 @@ public class UserInput {
 
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter total number of numbers");
+        log.info("Enter total number of numbers");
         int count = s.nextInt();
         s.nextLine(); // throw away the newline.
 
         Integer[] numbers = new Integer[count];
-        System.out.println("Enter numbers separated by space");
+        log.info("Enter numbers separated by space");
         try(Scanner numScanner = new Scanner(s.nextLine())) {
             for (int i = 0; i < count; i++) {
                 if (numScanner.hasNextInt()) {
                     numbers[i] = numScanner.nextInt();
                 } else {
-                    System.out.println(ERROR_INVALID_NUMBER);
+                    log.error(ERROR_INVALID_NUMBER);
                     throw new IllegalArgumentException(ERROR_INVALID_NUMBER);
                 }
             }
